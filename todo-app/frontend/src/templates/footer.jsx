e80bb6c55ff7fa680d6c276fdbcb20260ad3231d
Verônica SP <veronicasp54@gmail.com>
2020-04-08 21:58:47 -0300

    add footer

--- todo-app/frontend/src/templates/footer.jsx
+++ todo-app/frontend/src/templates/footer.jsx
@@ -4,8 +4,13 @@ export default props => (
   <div>
       <footer className="footer">
         <div className="rodape">
-          <p> <b> Endereço: Rua XXXX, 10 - Curitiba - Paraná | Telefone: 7070-7070 | Email: contato@terrasenandvelaris.com </b> </p>
-        </div>
+          <div className='left'>
+              <div className='copyrigth'> <p> <b> © Verônica Souza P. </b> </p></div>
+              <div><p>SAUN QD 5 LT B, Asa Norte, Brasília-DF, Brasil - CEP 70040-912</p></div>
+            </div>
+              <div className='middle'><p></p></div>
+              <div className='rigth'></div>
+          </div>
       </footer>
   </div>
 
